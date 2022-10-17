6d78af1fca1c263f3dae727efa8d0faf1574366b
MiguelX413 <lbpgaming27@gmail.com>
2022-10-17 13:21:26 -0700

    Add comments to Interval.is_disjoint()

--- src/lib.rs
+++ src/lib.rs
@@ -519,11 +519,13 @@ impl<Float: FloatT> Interval<Float> {
                 if (x.2 < y.1) | ((x.2 == y.1) & (!x.3 | !y.0)) {
                     break;
                 }
+                // Emulate max()
                 let left_bound = if (x.1 > y.1) | ((x.1 == y.1) & !x.0) {
                     (x.0, x.1)
                 } else {
                     (y.0, y.1)
                 };
+                // Emulate min()
                 let right_bound = if (x.2 < y.2) | ((x.2 == y.2) & !x.3) {
                     (x.2, x.3)
                 } else {
